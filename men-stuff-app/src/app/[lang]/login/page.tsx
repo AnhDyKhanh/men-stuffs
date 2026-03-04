6ffcfc15136e2ad0c586876d8ca13a9adccbaae2
Duy Khánh <duykhanh4239@gmail.com>
2026-03-04 17:13:15 +0700

    chore: fix linh tinh

--- men-stuff-app/src/app/[lang]/login/page.tsx
+++ men-stuff-app/src/app/[lang]/login/page.tsx
@@ -1,11 +1,24 @@
+'use client'
+
 import { LoginForm } from './_components/LoginForm'
 import { labels, BASE_PATH } from '@/lib/labels'
+import { useState } from 'react'
 
-type PageProps = {
-  params: Promise<{ lang: string }>
-}
+export default function LoginPage() {
+  const [open, setOpen] = useState(true)
 
-export default async function LoginPage({ params }: PageProps) {
-  await params
-  return <LoginForm dict={labels} basePath={BASE_PATH} />
+  return (
+    <div
+      className="min-h-screen w-full flex items-center justify-center bg-background"
+      style={{
+        // BỎ chữ /public đi, chỉ bắt đầu từ /bg
+        backgroundImage: `url('/bg/backGround.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <LoginForm basePath={BASE_PATH} open={true} onOpenChange={setOpen} />
+    </div>
+  )
 }
